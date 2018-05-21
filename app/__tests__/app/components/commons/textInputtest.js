4f5a97adc92e2bda374169a4028bf8954a52294c
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-05-21 09:09:08 +0200

    Add: Pierwszy test.

---
+++ app/__tests__/app/components/commons/textInputtest.js
@@ -0,0 +1,8 @@
+import TestUtils from 'react-dom/test-utils';
+
+describe('Jakies testy', ()=>{
+    it('dodawanie', ()=> {
+        const sum = 2 + 2;
+        expect(sum).toEqual(4);
+    });
+});
